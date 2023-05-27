74101086add86befd6ad095ae107eac12127e662
Simon Schubert <simon.schubert@htl-wels.at>
2023-05-27 13:10:53 -0400

    installed notify for notifications

--- lua/kali/options.lua
+++ lua/kali/options.lua
@@ -35,6 +35,8 @@ local options = {
     backup = false,                             --Creates a backup file.
 }
 
+vim.notify = require("notify")                  --Takes nvim-notify as the standard notification tool
+
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
