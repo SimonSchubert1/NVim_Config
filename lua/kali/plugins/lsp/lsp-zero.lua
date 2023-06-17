9c5bca4367ebcb97de54f026d8959a13dad99c29
Simon Schubert <simon.schubert@htl-wels.at>
2023-06-18 01:32:20 +0200

    some changes to debugging symbols

--- lua/kali/plugins/lsp/lsp-zero.lua
+++ lua/kali/plugins/lsp/lsp-zero.lua
@@ -29,12 +29,43 @@ return {
         {
             "mfussenegger/nvim-dap",
             config = function()
+                vim.fn.sign_define(
+                    "DapBreakpoint",
+                    { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" }
+                )
+                vim.fn.sign_define(
+                    "DapBreakpointCondition",
+                    { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" }
+                )
+                vim.fn.sign_define(
+                    "DapBreakpointRejected",
+                    { text = "", texthl = "", linehl = "", numhl = "" }
+                )
+                vim.fn.sign_define(
+                    "DapLogPoint",
+                    { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" }
+                )
+                vim.fn.sign_define(
+                    "DapStopped",
+                    { text = "", texthl = "GitSignsAdd", linehl = "", numhl = "" }
+                )
+
                 local dap = require("dap")
+                local dapui = require("dapui")
+                dap.listeners.after.event_initialized["dapui_config"] = function()
+                    dapui.open()
+                end
+                dap.listeners.before.event_terminated["dapui_config"] = function()
+                    dapui.close()
+                end
+                dap.listeners.before.event_exited["dapui_config"] = function()
+                    dapui.close()
+                end
             end,
         },
         {
             "rcarriga/nvim-dap-ui",
-            opts = function()
+            config = function()
                 require("dapui").setup()
             end,
         },
