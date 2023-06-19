2bb034cc4cf2a6210a7f8ed9c95d746814913c35
Simon Schubert <simon.schubert@htl-wels.at>
2023-06-19 22:11:46 +0200

    some changes to java config

--- lua/kali/plugins/lsp/init.lua
+++ lua/kali/plugins/lsp/init.lua
@@ -1,10 +1,11 @@
 return {
-	require("kali.plugins.lsp.lsp-zero"),
-	require("kali.plugins.lsp.completion"),
-	require("kali.plugins.lsp.null-ls"),
+    require("kali.plugins.lsp.lsp-zero"),
+    require("kali.plugins.lsp.completion"),
+    require("kali.plugins.lsp.null-ls"),
     require("kali.plugins.lsp.toggle-lsp-diagnostics"),
     require("kali.plugins.lsp.fidget"),
     require("kali.plugins.lsp.code-action-menu"),
     require("kali.plugins.lsp.lsp-signature"),
-	require("kali.plugins.lsp.handlers").setup(),
+    -- require("kali.plugins.lsp.coc"),
+    require("kali.plugins.lsp.handlers").setup(),
 }
