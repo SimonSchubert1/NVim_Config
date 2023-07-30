a337f8e338b17635198323a19bacd5acd7acf908
Simon Schubert <simon.schubert@htl-wels.at>
2023-07-30 22:15:36 +0200

    cleaned up something

--- lua/kali/plugins/init.lua
+++ lua/kali/plugins/init.lua
@@ -1,8 +1,3 @@
 return {
     -- My plugins here (this section is used to install plugins (this will be used in 99% of cases editing this file))
-    --Telescope Plugin installed here as long as I don't know how to install it in the same file as Telescope
-    "nvim-telescope/telescope-media-files.nvim",
-
-   -- Bufferline
-    "moll/vim-bbye",
 }
